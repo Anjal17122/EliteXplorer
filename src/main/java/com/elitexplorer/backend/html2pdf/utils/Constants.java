c6395047f3511e93fa24e8dca1a0350697040daf
Anjal Sapkota <david.sapkota123@gmail.com>
2023-06-15 12:11:35 +0545

    Committed

--- src/main/java/com/elitexplorer/backend/html2pdf/utils/Constants.java
+++ src/main/java/com/elitexplorer/backend/html2pdf/utils/Constants.java
@@ -4,18 +4,22 @@ import java.io.File;
 import java.nio.file.Paths;
 
 public class Constants {
-    public static final String imagePath = "C:\\Users\\Dell\\Desktop\\images";
-    public static final String fontPath = Paths.get("")
-            .toAbsolutePath()
-            .toString() +File.separator + "backend"+File.separator + "src" +File.separator +"main" +File.separator +"resources" +File.separator +"static" +File.separator +"fonts";
+//    public static final String imagePath = "C:\\Users\\Dell\\Desktop\\images";
+//    public static final String fontPath = Paths.get("")
+//            .toAbsolutePath()
+//            .toString() +File.separator + "backend"+File.separator + "src" +File.separator +"main" +File.separator +"resources" +File.separator +"static" +File.separator +"fonts";
 
     public static final String imageCopySource = "C:\\Users\\Dell\\Desktop\\backend\\backend\\src\\main\\resources\\static\\uploads";
 
+
     public static final String imageCopyDest = "C:\\Users\\Dell\\Desktop\\images";
 
+// Apple Path
+// public static final String imagePath = "/Users/Documents/Desktop/images";
+// public static final String fontPath = "/Users/Documents/Desktop/fonts";
 
-//    public static final String imagePath = "/Users/username/Desktop/images;
-//    public static final String fontPath = "/Users/username/Desktop/backend/backend/src/main/resources/static/fonts";
-
+//    Windows Path
+    public static final String imagePath = "C:\\Users\\Dell\\Desktop\\images";
+    public static final String fontPath = "C:\\Users\\Dell\\Desktop\\fonts";
 
 }
